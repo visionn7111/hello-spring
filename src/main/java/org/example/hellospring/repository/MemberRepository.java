25bb038aa7d0be57a4c6a00511083ac40efd593a
visionn7111 <visionn7111@gmail.com>
2025-06-22 12:15:45 +0900

    commit

--- src/main/java/org/example/hellospring/repository/MemberRepository.java
+++ src/main/java/org/example/hellospring/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    Memver save(Member member);
+    Member save(Member member);
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
     List<Member> findAll();
--- src/main/java/org/example/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/org/example/hellospring/repository/MemoryMemberRepository.java
@@ -1,10 +1,7 @@
 package org.example.hellospring.repository;
 
 import org.example.hellospring.domain.Member;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository {
 
@@ -12,22 +9,30 @@ public class MemoryMemberRepository implements MemberRepository {
     private static long sequence = 0L;
 
     @Override
-    public Memver save(Member member) {
-        return null;
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
     }
 
     @Override
     public Optional<Member> findById(Long id) {
-        return Optional.empty();
+        return Optional.ofNullable(store.get(id));
     }
 
     @Override
     public Optional<Member> findByName(String name) {
-        return Optional.empty();
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
     }
 
     @Override
     public List<Member> findAll() {
-        return List.of();
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 }
